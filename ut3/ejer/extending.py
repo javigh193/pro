3e3f5f5d66bd13e1bdea54143b5e9d91afd48ba1
Javier-Garcia-Hernandez <javigh1903@movistar.es>
2022-11-18 17:37:52 +0000

    se corrigen ejercicios

--- ut3/ejer/extending.py
+++ ut3/ejer/extending.py
@@ -1,8 +1,28 @@
-values = [10, [50, 60, 70]]
+# values = [10, [50, 60, 70]]
+# flattened_values = []
+# for value in values:
+#     if type(value) is list:
+#         flattened_values.extend(value)
+#     else:
+#         flattened_values.append(value)
+# print(flattened_values)
+
+values = [10, [50, [49, 67], 70]]
 flattened_values = []
+# supongo que es flat hasta que se demuestre lo contrario
+flat = True
 for value in values:
     if type(value) is list:
-        flattened_values.extend(value)
-    else:
-        flattened_values.append(value)
+        flat = False
+while not flat:
+    for value in values:
+        if type(value) is list:
+            flattened_values.extend(value)
+        else:
+            flattened_values.append(value)
+    # supongo que es flat hasta que se demuestre lo contrario
+    flat = True
+    for value in flattened_values:
+        if type(value) is list:
+            flat = False
 print(flattened_values)
